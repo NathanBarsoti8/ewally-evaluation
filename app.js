feb6a0699efad700607905644ac266188998f5a5
Nathan Barsoti <nathanbarsoti@hotmail.com>
2022-04-04 14:52:23 -0300

    adjust swagger position

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ app.use(bodyParser.text())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({ type: 'application/json' }))
 
-routes(app)
-
-app.listen(port, () => {
-    console.log(`API LISTENING ON PORT ${port}`)
-})
-
 //swagger
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
 
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+routes(app)
+
+app.listen(port, () => {
+    console.log(`API LISTENING ON PORT ${port}`)
+})
 
 module.exports = { app }
