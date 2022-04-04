1bba6fbc78cfc37652be244e1b252dc8148fa1db
Nathan Barsoti <nathanbarsoti@hotmail.com>
2022-04-04 14:41:25 -0300

    add default message

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,5 @@ module.exports = (app) => {
     const basev1 = '/api/v1'
 
     app.use(`${basev1}/bills`, require('./bills'))
+    app.use('/', (req, res) => { return res.send("API to consult bank slip and payment of insurances") })
 }
